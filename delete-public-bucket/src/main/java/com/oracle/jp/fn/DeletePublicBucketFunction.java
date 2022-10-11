a4b8cfebe72a0ae75638ecad525b21ceaf9ec8ad
shukawam <shukawam@gmail.com>
2022-10-11 18:44:53 +0900

    WIP

--- delete-public-bucket/src/main/java/com/oracle/jp/fn/DeletePublicBucketFunction.java
+++ delete-public-bucket/src/main/java/com/oracle/jp/fn/DeletePublicBucketFunction.java
@@ -6,12 +6,18 @@ import com.oracle.bmc.objectstorage.requests.DeleteBucketRequest;
 import com.oracle.bmc.objectstorage.responses.DeleteBucketResponse;
 import com.oracle.jp.fn.data.AuditLogInput;
 
+import java.util.List;
+
 /**
  * @author shukawam
  */
 public class DeletePublicBucketFunction {
 
-    public boolean handleRequest(AuditLogInput input) {
+    public boolean handleRequest(List<AuditLogInput> inputs) {
+        if (inputs.size() != 1) {
+            return false;
+        }
+        AuditLogInput input = inputs.get(0);
         if ("ObjectRead".equals(input.logContent.data.additionalDetails.publicAccessType) ||
                 "ObjectReadWithoutList".equals(input.logContent.data.additionalDetails.publicAccessType)) {
             String namespaceName = input.logContent.data.additionalDetails.namespace;
