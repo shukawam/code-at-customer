3d18879e8b75405612723170b73a78848f962f9f
shukawam <shukawam@gmail.com>
2022-10-12 14:50:08 +0900

    Test jackson ignore properties

--- delete-public-bucket/src/main/java/com/oracle/jp/fn/data/AuditLogInput.java
+++ delete-public-bucket/src/main/java/com/oracle/jp/fn/data/AuditLogInput.java
@@ -1,15 +1,18 @@
 package com.oracle.jp.fn.data;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 /**
  * @author shukawam
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuditLogInput {
     public Data data;
-    public String dataschema;
-    public String id;
-    public Object oracle;
-    public String source;
-    public String specversion;
-    public String time;
-    public String type;
+//    public String dataschema;
+//    public String id;
+//    public Object oracle;
+//    public String source;
+//    public String specversion;
+//    public String time;
+//    public String type;
 }
