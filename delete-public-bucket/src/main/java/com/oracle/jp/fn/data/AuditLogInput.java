ac67e24fa38a7569db63660a9d91471fc44b1653
shukawam <shukawam@gmail.com>
2022-10-12 14:43:30 +0900

    WIP

--- delete-public-bucket/src/main/java/com/oracle/jp/fn/data/AuditLogInput.java
+++ delete-public-bucket/src/main/java/com/oracle/jp/fn/data/AuditLogInput.java
@@ -5,4 +5,11 @@ package com.oracle.jp.fn.data;
  */
 public class AuditLogInput {
     public Data data;
+    public String dataschema;
+    public String id;
+    public Object oracle;
+    public String source;
+    public String specversion;
+    public String time;
+    public String type;
 }
