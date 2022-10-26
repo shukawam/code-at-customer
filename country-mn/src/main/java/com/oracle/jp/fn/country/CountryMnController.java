6edc5b6a62b0092b0003b338a81fbad1cb927cde
shukawam <shukawam@gmail.com>
2022-10-26 18:07:32 +0900

    Initial commit

--- country-mn/src/main/java/com/oracle/jp/fn/country/CountryMnController.java
+++ country-mn/src/main/java/com/oracle/jp/fn/country/CountryMnController.java
@@ -26,29 +26,13 @@ public class CountryMnController {
 
     @Get("/country")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Country> getAllCountry() throws InterruptedException, IOException {
+    public Object getAllCountry() throws InterruptedException, IOException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s/country", ordsConfig.getEndpoint())))
                 .GET()
                 .build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Gson gson = new Gson();
-        List<Country> countryList = Arrays.asList(gson.fromJson(response.body(), Country[].class));
-        return countryList;
-    }
-
-    @Get("/country/id/{countryId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Country getCountryByCountryId(String countryId) throws InterruptedException, IOException  {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(String.format("%s/country/id/%s", ordsConfig.getEndpoint(), countryId)))
-                .GET()
-                .build();
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Gson gson = new Gson();
-        Country country = gson.fromJson(response.body(), Country.class);
-        return country;
+        return response.body();
     }
 }
